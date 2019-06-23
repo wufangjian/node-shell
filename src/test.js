a776acdddc0ac2a8d0e350b8dc5cf8ad55d432f5
wufangjian <wufangjian@bytedance.com>
2019-06-23 23:56:33 +0800

    update

---
+++ src/test.js
@@ -0,0 +1,9 @@
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-C, --chdir <path>', 'change the working directory')
+  .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+  .option('-T, --no-tests', 'ignore test hook');
+  
+program.parse(process.argv)
