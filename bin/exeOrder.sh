c7cba27c8d12dcb9a3ddaad0428b22944dbf4e86
wufangjian <wufangjian@bytedance.com>
2019-06-24 11:28:33 +0800

    feat: 添加脚本

---
+++ bin/exeOrder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "please input firstname  and lastname"
+read -p "please input firstname: " firstname
+read -p "please input lastname: " lastname
+echo -e "\n full name is: $firstname $lastname"
